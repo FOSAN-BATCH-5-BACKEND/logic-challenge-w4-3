0b6ab7e8f7ac52d9755a9d4898d075cbe8b0f44d
Rijal Fadhila <r3dlabelz@gmail.com>
2021-05-14 03:43:59 +0700

    Minal Aidzin Wal Faidzin, Mohon Maaf Lahir & Bathin

---
+++ autoTempa.js
@@ -0,0 +1,61 @@
+/*
+INSTRUKSI
+=========
+BUATLAH BERURUTAN SESUAI LEVEL AGAR MEMUDAHKAN SAAT MENGERJAKAN
+
+## level 0
+Buatlah sebuah fungsi untuk melakukan tempa senjata otomatis dengan parameter jumlah tempa nya,
+lalu dengan menggunakan Math.random dan Math.floor,
+buatlah pengkondisian tempa 90% berhasil dan 10% gagal
+
+## level 1
+jika tempa nya berhasil, maka senjata akan +1,
+dan jika tempanya gagal, maka senjata akan -1,
+jika status senjata adalah +0, maka senjata tidak akan -1
+
+-- console.log untuk setiap perulangan --
+`SUKSES, pedang anda sekarang +3`
+`GAGAL, pedang anda sekarang +1`
+
+## level 2
+modifikasi peluang, sehingga peluang berhasil nya akan turun 10% setiap plus senjata
+(90% saat +0, 80% saat +1, 70% saat +2, dst), 
+tambahkan persentase chance nya pada log
+
+--console.log untuk setiap perulangan --
+(chance 90%) SUKSES, pedang anda sekarang +1
+(chance 80%) SUKSES, pedang anda sekarang +2
+(chance 70%) SUKSES, pedang anda sekarang +3
+(chance 60%) SUKSES, pedang anda sekarang +4
+(chance 50%) GAGAL, pedang anda sekarang +3
+(chance 60%) SUKSES, pedang anda sekarang +4
+(chance 50%) GAGAL, pedang anda sekarang +3
+(chance 60%) SUKSES, pedang anda sekarang +4
+(chance 50%) SUKSES, pedang anda sekarang +5
+(chance 40%) GAGAL, pedang anda sekarang +3
+
+
+## level 3
+Jika senjata antara +3 sampai +5, dan mengalami kegagalan saat ditempa,
+maka tidak akan -1, tapi akan langsung menjadi +3,
+dan jika senjata  +6, dan mengalami kegagalan saat menempa senjata akan HANCUR
+
+--console.log pada saat gagal menempa +6 dan HANCUR --
+`(chance 30%) UPGRADE GAGAL, SENJATA TELAH HANCUR`
+
+## level 4
+jika senjata sudah berhasil ditempa sampai dengan +7,
+maka senjata tidak bisa ditempa lagi dan memberikan pesan "SELAMAT SENJATA ANDA SUDAH MAKSIMAL +7"
+
+--console.log saat berhasil menempa MAKSIMAL --
+(chance 30%) SUKSES, SELAMAT SENJATA ANDA SUDAH MAKSIMAL +7`
+*/
+
+
+function autoTempa(jumlahTempa){
+    // buat kodemu disini
+    
+}
+
+
+autoTempa(10);
